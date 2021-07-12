e8cfd2332dd5f6e57b74a461a060e64dc7e5b2a6
averichev <averichev@yandex.ru>
2021-07-12 13:45:32 +0300

    style: Namespace

--- isogram/Isogram.cs
+++ isogram/Isogram.cs
@@ -1,4 +1,4 @@
-﻿using Models;
+﻿using Isogram.Models;
 
 namespace Isogram
 {
--- isogram/Models/Alphabet.cs
+++ isogram/Models/Alphabet.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Models
+namespace Isogram.Models
 {
     public class Alphabet
     {
