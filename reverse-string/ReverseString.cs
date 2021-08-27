5744edb11a600d1282bc74d0a35d06f47bf104d4
averichev <averichev@yandex.ru>
2021-08-27 10:59:25 +0300

    feat: Reverse string

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,13 @@
+﻿public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var result = "";
+        foreach (var character in input)
+        {
+            result = character + result;
+        }
+
+        return result;
+    }
+}
