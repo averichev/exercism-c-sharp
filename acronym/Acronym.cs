72b2d0aafe1cad69f8c6885dfae87a85a821ee78
averichev <averichev@yandex.ru>
2021-08-27 10:58:44 +0300

    style: Acronym

--- acronym/Acronym.cs
+++ acronym/Acronym.cs
@@ -1,22 +1,20 @@
 ﻿using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Acronym
+
+public static class Acronym
 {
-    public static class Acronym
+    public static string Abbreviate(string phrase)
     {
-        public static string Abbreviate(string phrase)
-        {
-            const string pattern = "[^a-zA-Z']";
-            string[] words = Regex.Split(
-                phrase,
-                pattern,
-                RegexOptions.IgnoreCase
-            );
-            var filtered = words.Where(word => !string.IsNullOrEmpty(word));
-            var result = filtered
-                .Aggregate("", (current, word) => current + word[0].ToString().ToUpper());
-            return result;
-        }
+        const string pattern = "[^a-zA-Z']";
+        string[] words = Regex.Split(
+            phrase,
+            pattern,
+            RegexOptions.IgnoreCase
+        );
+        var filtered = words.Where(word => !string.IsNullOrEmpty(word));
+        var result = filtered
+            .Aggregate("", (current, word) => current + word[0].ToString().ToUpper());
+        return result;
     }
 }
